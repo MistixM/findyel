f23b03e957b9bf9f3722367a75cb5b9c95b6e3de
MDEV <108402379+MistixM@users.noreply.github.com>
2024-12-30 18:28:32 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from utils.check_site import get_page_speed_number
-
-from DrissionPage import ChromiumPage
-
-driver = ChromiumPage()
-
-speed = get_page_speed_number('https://pagespeed.web.dev/analysis/http-letsremotivate-com/ekopitrhu9?form_factor=mobile', driver)
-
-print(speed['mobile'])
